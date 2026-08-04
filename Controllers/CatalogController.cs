dd88e66f6ea41c84ddcc3cbb7276783e2c5bb8e5
Sunz403 <sunzsimz1@gmail.com>
2026-08-04 13:59:22 +0200

    Added Redirect Link

--- Controllers/CatalogController.cs
+++ Controllers/CatalogController.cs
@@ -1,6 +1,7 @@
 using AIShoppingAssistant.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AIShoppingAssistant.Controllers;
 
@@ -16,5 +17,12 @@ public class CatalogController : Controller
         ViewBag.Categories = _context.Products.Select(p => p.Category).Distinct().OrderBy(x => x).ToList();
         ViewBag.Stores = _context.Products.Select(p => p.StoreName).Distinct().OrderBy(x => x).ToList();
         return View();
+    }
+
+    [HttpGet("/Catalog/Details/{id:int}")]
+    public async Task<IActionResult> Details(int id, CancellationToken cancellationToken)
+    {
+        var product = await _context.Products.AsNoTracking().SingleOrDefaultAsync(product => product.Id == id, cancellationToken);
+        return product is null ? NotFound() : View(product);
     }
 }
